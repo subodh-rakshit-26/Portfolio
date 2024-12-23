1e5499b0af2d3af63417b86f51ebd8678c44f64f
subodh-rakshit-26 <rakshit26subodh@gmail.com>
2024-12-23 17:33:44 +0530

    small changes

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -1,11 +1,11 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function GET(request) {
-  return NextResponse.json({
-    success: true,
-    message: 'hle!',
-    data: {
-      message: 'Message and email sent successfully!',
-    }
-  }, { status: 200 });
-};
+// export async function GET(request) {
+//   return NextResponse.json({
+//     success: true,
+//     message: 'hle!',
+//     data: {
+//       message: 'Message and email sent successfully!',
+//     }
+//   }, { status: 200 });
+// };
